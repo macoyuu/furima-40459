c3a6a3bd4f89703cd62d0b550117641d0f322b70
macoyuu <165467533+macoyuu@users.noreply.github.com>
2024-06-18 19:48:32 +0900

    ユーザー管理機能(修正2)

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname { Faker::Internet.username }
     email { Faker::Internet.email }
-    password { Faker::Internet.password(min_length: 6) }
+    password {'1a' + Faker::Internet.password(min_length: 6)}
     password_confirmation { password }
     first_name { '山田' }
     last_name { '一郎' }
