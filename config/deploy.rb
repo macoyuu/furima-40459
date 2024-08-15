695d7bca9b9f3b5148c507b41b1bd4016b7272fa
macoyuu <165467533+macoyuu@users.noreply.github.com>
2024-08-15 20:36:08 +0900

    デプロイ自動化(１)

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,22 @@
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock 'Capistranoのバージョン'
+lock '3.19.1'
 
 # Capistranoのログの表示に利用する
-set :application, 'ご自身のアプリケーション名'
+set :application, 'furima-40459'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:Githubのユーザー名/レポジトリ名.git'
+set :repo_url,  'git@github.com:macoyuu/furima-40459.git'
 set :branch, 'main'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, 'このアプリで使用しているrubyのバージョン' #カリキュラム通りに進めた場合、’3.2.0’ です
+set :rbenv_ruby, '3.2.0' #カリキュラム通りに進めた場合、’3.2.0’ です
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                                  keys: ['~/.ssh/ご自身のキーペア名.pem'] 
+                                  keys: ['~/.ssh/my-key-pair.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
